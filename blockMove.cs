0be991331d3bc10184acac8c500a9a69b5a91903
RossGalbraith <45493728+RossGalbraith@users.noreply.github.com>
2021-02-19 13:29:42 +0000

    Add files via upload
    Basic controls and camera tracking

---
+++ blockMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blockMove : MonoBehaviour
+{
+    public float moveSpeed;
+    public float turnSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 8f;
+        turnSpeed = 0.8f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * turnSpeed * Input.GetAxis("Horizontal") * Time.deltaTime);
+        transform.Translate(-moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime);
+    }
+}
