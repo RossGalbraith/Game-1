1bf7594d3b96e37ae7b44ecb6fc0720f1c7fcef3
RossGalbraith <45493728+RossGalbraith@users.noreply.github.com>
2021-02-22 19:27:49 +0000

    More code to give life to the game
    Collisions, AI, player being able to jump, etc.

---
+++ enemyChase.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyChase : MonoBehaviour
+{
+    public Transform Player;
+    int MoveSpeed = 4;
+    int MaxDist = 10;
+    int MinDist = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Player);
+        
+        if(Vector3.Distance(transform.position, Player.position) >= MinDist)
+        {
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+            if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+            {
+
+            }
+        }
+    }
+}
---
+++ playerCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class playerCollision : MonoBehaviour
+        
+{
+    [SerializeField] private float knockbackStrength;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody rb = collision.collider.GetComponent<Rigidbody>();
+
+        if(rb != null)
+        {
+            Vector3 direction = collision.transform.position - transform.position;
+            direction.y = 0; //remove if player wants to go up when knocked back
+
+            rb.AddForce(direction.normalized * knockbackStrength, ForceMode.Impulse);
+        }
+    }
+}
